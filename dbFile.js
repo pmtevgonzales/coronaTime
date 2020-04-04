9224c53c58bdd81ba988789b482dcdf84233a168
pmtevgonzales <patvalentinegonzales@gmail.com>
2020-04-05 01:39:10 +1300

    add object from JSON to the database

--- dbFile.js
+++ dbFile.js
@@ -6,13 +6,13 @@ const connection = require('knex')(config)
 const fetch = require('node-fetch')
 
 module.exports = {
-    getLatestData,
-    getCountries,
+    getTimeseriesFromJSON,
+    getCountriesFromJSON,
     saveLatestData,
     initialiseCountry
 }
-
-function getLatestData() {
+//to have the JSON file as object 
+function getTimeseriesFromJSON() {
     //this url is the data from Johns Hopkins CSSE regarding COVID-19 that transforms into a json file updated daily.
     let url = "https://pomber.github.io/covid19/timeseries.json"
     let settings = { method: "Get" }
@@ -21,23 +21,39 @@ function getLatestData() {
         .then(res => res.json())
 }
 
-function getCountries() {
+function getCountriesFromJSON() {
     let url = "https://pomber.github.io/covid19/countries.json"
     let settings = { method: "Get" }
 
     return fetch(url, settings)
         .then(res => res.json())
-        // .then((json) => {
-
-        // })
 }
 
-function saveLatestData(casesByCountry) {
-    casesByCountry.then((c) => {
-        var countries = Object.keys(c)
-        countries.forEach((country) => {
-            console.log(country)
-        }) 
+//insert the object from the JSON to the database/sqlite
+function saveLatestData(casesByCountry, db = connection) {
+    return db('timeseries').del()
+    .then(function () {
+        casesByCountry.then((c) => {
+            var latestData = Object.keys(c)
+            latestData.forEach((country) => {
+                db('country')
+                .where('country', country)
+                .first()
+                .then((countryRow) => {
+                    c[country].forEach ((data) => {
+                        //query of the existing if statement??? for stretch
+                        db('timeseries').insert({
+                            country_id: countryRow.id,
+                            case_date: data.date,
+                            confirmed_cases: data.confirmed,
+                            deaths: data.deaths,
+                            recovered: data.recovered
+                        })
+                        .then()
+                    })
+                })
+            })
+        })
     })
 }
 
@@ -57,8 +73,3 @@ function initialiseCountry(db = connection) {
         })
     })
 }
-
-initialiseCountry()
-// var latestData = getLatestData()
-
-// saveLatestData(latestData)
